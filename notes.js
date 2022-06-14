ed996be886d7521e1a9384b5aaef9b7cb8dee2cb
Alec Venida <alecvenida@gmail.com>
2022-06-13 17:05:43 -0700

    first react function

--- notes.js
+++ notes.js
@@ -1,1 +1,16 @@
 // REACT LET'S GO
+
+
+//first jsx function
+function NavBar() {
+  return (
+    <nav>
+      <h1>Pett App</h1>
+      <ul>
+        <li className="selected">
+            <a href="/pets">Pets</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
