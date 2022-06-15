b78c5a1a605a43e908dbf5173b667b209b47937e
Alec Venida <alecvenida@gmail.com>
2022-06-14 21:42:47 -0700

    let's go useEffect

--- we-for-wednesday-notes.js
+++ we-for-wednesday-notes.js
@@ -20,3 +20,21 @@ function App() {
   );
 };
 export default App
+
+
+import { useEfect, useEffect} from 'react';
+
+const Main = () => {
+  useEffect(() => {
+    console.log('useEffect1 Run')
+  });
+
+  return (
+    <div>
+      {console.log('rendered or re-rendered')}
+      <h1>Main Component</h1>
+    </div>
+  );
+};
+
+export default Main;
