c050a47d5d58338cfcb7fb0780ad3b40a77c45b6
Alec Venida <alecvenida@gmail.com>
2022-06-20 15:13:17 -0700

    some redux notes, when to use redux vs context

--- react-App-practice-Monday/wk15/hw-for-tues-redux.js
+++ react-App-practice-Monday/wk15/hw-for-tues-redux.js
@@ -1,1 +1,5 @@
 //some notes let's go redux
+
+//for projects with simpler global state, context is easier than redux.
+//if it's more complicated, use redux.
+//for projects with more sophisticated global state requirements, Redux remains a popular option. Redux offers greater flexibility with support for middleware and richer developer tools in the form of the Redux DevTools.
