9732b2c2a3f2523d9d478720ef14f15c966c8f4a
Alec Venida <alecvenida@gmail.com>
2022-06-20 12:49:30 -0700

    week 15 let's go

---
+++ react-App-practice-Monday/wk14/hw-for-wednesday-notes.js
@@ -0,0 +1,85 @@
+//src/Main.js
+const Main = () => {
+  return (
+    <div>
+      <h1>Main Component</h1>
+    </div>
+  );
+};
+
+
+//src App.js
+
+import Main from './Main';
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
+export default App
+
+
+import { useEfect, useEffect} from 'react';
+
+// const Main = () => {
+  useEffect(() => {
+    console.log('useEffect1 Run')
+  });
+
+  return (
+    <div>
+      {console.log('rendered or re-rendered')}
+      <h1>Main Component</h1>
+    </div>
+  );
+// };
+
+// export default Main;
+
+
+const [toggleOne, setToggleOne] = useState(false)
+
+{/* <button onClick={() => setToggleOne(!toggleOne)}>ToggleOne</button> */}
+
+
+//useEfect with an epty dependency array
+
+// only run one time, directly after the first render
+
+useEffect(() => {
+  console.log('UseEffect1 Ran');
+}, []); // empty dependency array as second argument
+
+
+const [toggleTwo, setToggleTwo] = useState(false);
+
+useEffect(() => {
+  console.log('UseEffect2 Ran');
+}, [toggleTwo]);
+
+useEffect(() => {
+  console.log('UseEffect2 Ran');
+  if(toggleTwo)
+    console.log('toggleTwo sliece of state istrue so this code runs')
+}, [toggleTwo]);
+
+
+//optional cleanup function
+
+const [count, setCount] = useState(0)
+
+useEffect(() => {
+  setInterval(() => {
+    console.log(`UseEffect3 with interval number ${count} is running`)
+  }, 1000)
+}, [count]);
+
+return () => {
+  console.log(
+    `Use Effect3 cleanup ran.\nsetInterval number ${count} is being cleared out`
+  );
+  clearInterval(myInterval);
+};
---
+++ react-App-practice-Monday/wk15/notes-d1.js
@@ -0,0 +1,6 @@
+/**
+ * class components is a react component defined using es6 classes
+ *
+ * before 16.8 of react, only components allowed to use hold state
+ * 16.8 hooks are introduced, and can hold state.
+ */
