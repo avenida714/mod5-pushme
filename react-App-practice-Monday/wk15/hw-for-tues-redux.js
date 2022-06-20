9a1c60383a8ccc02d9f3260f5de619e4aad66e09
Alec Venida <alecvenida@gmail.com>
2022-06-20 15:38:09 -0700

    reducer example, update the store, create a store

--- react-App-practice-Monday/wk15/hw-for-tues-redux.js
+++ react-App-practice-Monday/wk15/hw-for-tues-redux.js
@@ -22,3 +22,46 @@
 //TIME TRAVELLING DEV TOOLS    - can look back through the history of the state and toggle past actions on and off to see a live recalc of the state. this ability to revert to a previous state is what is meant by time travel.
 
 //THUNKS    traditional approach to middleware parallels the format of reducers. THUNKS are an alternate approach. refers to a func whose primary purpose is simply to call another func.  in redux, a thunk aciton creator returns a func (not an obj). used most commonly to make async API reqs.
+
+//*******************************STORE *******************************/
+
+
+//STORE API --  an obj that holds the app state, wrapped in a minimalist API.
+//3 methods:
+//1 getState - returns the store's current state
+//2 dispatch(action) - passes action into the store's reducer telling it what info to update.
+//3 subscribe(callback) - registers a callback to trigger whenever the store updates. returns a func, when infoked, unsubscribes the callback func form the store.
+
+
+//create a store
+import {createStore} from 'redux';
+
+const store = createStore(reducer, preloadedState, enhancer);
+
+//    reducer (required) - A reducing function that receives the store's current state and incoming action, determines how to update the store's state, and returns the next state (more on this in a moment).
+// preloadedState (optional) - An object representing any application state that existed before the store was created.
+// enhancer (optional) - A function that adds extra functionality to the store.
+
+
+//update the store
+store.dispatch(action);
+//action = POJO with type key indicating which action to be performed.   payload is optional, keys contain any new info
+const addOrange = {
+  type: 'ADD_FRUIT',  // this is the type
+  fruit: 'orange',  // this is the payload
+};
+
+
+//REDUCER EXAMPLE
+const reducer = (state = {}, aciton) => {
+  switch (action.type) {
+    case 'ACTION_TYPE_ONE':
+      return {type: 1};
+    case 'ACTION_TYPE_TWO':
+      return {type: 2};
+    case 'ACTION_TYPE_RESET':
+      return {};
+    default:
+      return state;
+  }
+};
