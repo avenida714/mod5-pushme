335517af32cb9b1e8cf23f3d750f369356e98855
Alec Venida <alecvenida@gmail.com>
2022-06-20 12:52:04 -0700

    class components legacy notes

--- react-App-practice-Monday/wk15/notes-d1.js
+++ react-App-practice-Monday/wk15/notes-d1.js
@@ -3,4 +3,15 @@
  *
  * before 16.8 of react, only components allowed to use hold state
  * 16.8 hooks are introduced, and can hold state.
+ *
+ * slightly slower than functional
+ *
+ * logic of class components can sometimes be trickier
+ *
+ * why learn them?  need to be known for legacy code; code for businesses who have not completely refactored their codebase.
+ * might need to know how to migrate from class to function
+ *
+ * class components are widely used in interviews.
+ *
+ *
  */
