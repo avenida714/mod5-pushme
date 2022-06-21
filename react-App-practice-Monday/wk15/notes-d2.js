2fa18343ec4371fda17248caa1ef42b42fc9ec96
Alec Venida <alecvenida@gmail.com>
2022-06-21 09:08:15 -0700

    createStore, applyMiddleware, compose, combineReducers, enhancer

--- react-App-practice-Monday/wk15/notes-d2.js
+++ react-App-practice-Monday/wk15/notes-d2.js
@@ -1,1 +1,17 @@
 //store
+
+
+// getState  returns the store's current state
+
+// dispatchEvent(action)  passes action into the store's reducer
+
+// subscribe - registers a callback to be triggered whenever the store updates. returns a unc, unsubscribes the cb func from the store.
+
+
+//set up the react/redux store
+
+//  import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+
+// const rootReducer = combineReducers({})   // need to have at least 1 reducer in order to set up and use createStore
+
+// let enhancer
