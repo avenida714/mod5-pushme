96071bc91f6d5d4658484c12375c41fa574f1cc6
Alec Venida <alecvenida@gmail.com>
2022-06-21 20:01:27 -0700

    front and back in the root

--- react-App-practice-Monday/wk15/hw-for-wed.js
+++ react-App-practice-Monday/wk15/hw-for-wed.js
@@ -7,4 +7,8 @@
 
 //thunks are used by higher order funcs. used to add asynchronous functionality to redux, allowing us to make calls to our backend server and then dispatch that info to our Action Creator
 
-//
+//PROJECT STRUCTURE
+// before, one folder held everything in the project
+
+//but now we are running frontend and backend. they need to talk to one another.  WE NEED ONE ROOT FOLDER.
+  //inside the root, there's a backend and a frontend.  backend will have all the express and psql; frontend will have react
